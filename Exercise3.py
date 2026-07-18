625b298f45dfaf6acbb987ee2997196195868784
Juan Carlos Valencia <valenciaju32@gmail.com>
2026-07-18 14:16:51 -0400

    Exercise 3: weeks to days conversion

---
+++ Exercise3.py
@@ -0,0 +1,5 @@
+# Exercise 3: Simple Conversion
+weeks_until_deadline = 16
+days_until_deadline = weeks_until_deadline * 7
+print(f"{weeks_until_deadline} weeks is equal to {days_until_deadline} days.")
+
