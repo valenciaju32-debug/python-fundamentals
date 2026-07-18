297cc1f83383421ff7e98af4280d7dd378f6affc
Juan Carlos Valencia <valenciaju32@gmail.com>
2026-07-17 20:09:51 -0400

    Day1:variables, data types, basic math

---
+++ day1.py
@@ -0,0 +1,10 @@
+# Day 1: Variables and basic opertors
+num1 = 15
+num2 = 4
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+print("Quotient:", num1 / num2)
+name= "Juan Carlos"
+years_experience = 13
+print(f"My name is {name} and I have {years_experience} years of professional experience.")
