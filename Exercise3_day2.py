b5057dc07adac59bdd83cdca4b575a034ee098c7
Juan Carlos Valencia <valenciaju32@gmail.com>
2026-07-18 18:53:39 -0400

    Exercise 3 Day 2: combining conditions with and

---
+++ Exercise3_day2.py
@@ -0,0 +1,7 @@
+# Exercise 3 Day 2: Combine Conditions with and/or 
+is_manager = True
+years_experience = 2
+if is_manager and years_experience >= 5:
+    print ("Eligible for leadership program")
+else:   
+    print ("Not yet eligible")
