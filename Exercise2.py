21726ae9c6ca71bbc7469006706016feb35af6e2
Juan Carlos Valencia <valenciaju32@gmail.com>
2026-07-18 13:57:08 -0400

    Exercise 2: personal profile

---
+++ Exercise2.py
@@ -0,0 +1,5 @@
+# Exercise 2: Personal Profile 
+Name= "Juan Carlos Valencia"
+Current_job_title= "Sr. Account Manager"
+year_of_experience= 20
+print(f"My name is {Name} and my current title is {Current_job_title} and I have a total of {year_of_experience} years of experience.")
