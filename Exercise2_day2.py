7776a7d9d439d4b8cabb91df5ad38401d2b23dd0
Juan Carlos Valencia <valenciaju32@gmail.com>
2026-07-18 18:27:24 -0400

    Exercise 2 Day 2: access level checker

---
+++ Exercise2_day2.py
@@ -0,0 +1,8 @@
+# Exercise 2: Access Level Checker
+years_experience = 1
+if years_experience >= 10:  
+    print ("Senior level access granted")
+elif years_experience >= 3:
+    print ("Mid level access granted")
+else: 
+    print ("Junior level access granted")
